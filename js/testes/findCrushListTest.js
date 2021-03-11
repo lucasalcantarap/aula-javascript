c23bc8fbeb3fd809fb69c22d73074dbdede7e593
lucasalcantarap <lucaspe0685@gmail.com>
2021-03-10 21:20:25 -0300

    criação de teste

---
+++ js/testes/findCrushListTest.js
@@ -0,0 +1,15 @@
+const should = require("should");
+const findCrushs = require("./findCrushs");
+
+describe("find crush", () => {
+  it("soma crushs", () => {
+    findCrushs.somaCrush(2, 3).should.be.equal(5);
+  });
+  it("deve somar dois números e retornar 30", () => {
+    findCrushs.somaCrush(10, 20).should.be.equal(30);
+  });
+  it('Passar um nome deve retornar mensagem de pessoa não encontrada', () =>{
+      findCrushs.encontra
+  } )
+});
+//38:42
---
+++ js/testes/findCrushs.js
@@ -0,0 +1,5 @@
+const somaCrush =  (n1, n2) => n1 + n2
+
+module.exports = {
+    somaCrush
+}
